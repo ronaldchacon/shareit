a69f0ed54d8f6d04b7314a309c54b542af9035a1
Ronald Chacon <ronald.chacon87@gmail.com>
2015-12-02 14:54:35 -0800

    Implement User model and controllers.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,14 @@
 class WelcomeController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  layout :choose_layout
+
   def index
   end
 
   def dashboard
+  end
+
+  def choose_layout
+    user_signed_in? ? "angular" : "application"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
   devise_for :users, controller: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  get '/dashboard' to: 'welcome#dashboard'
+  devise_scope :user do
+    get '/api/current_user' => 'users/sessions#show_current_user', as: "show_current_user"
+    post '/api/check/is_user' => 'users/users#is_user', as: "is_user"
+  end
+
+  get '/dashboard' => 'welcome#dashboard'
   root to: 'welcome#index'
 end
