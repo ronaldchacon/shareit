ef8fc24cdfb8fc1982007b7b413b918cc830f32d
Ronald Chacon <ronald.chacon87@gmail.com>
2015-12-03 12:11:05 -0800

    Implement angular share and current_user services

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,8 +1,13 @@
-angular.module("myApp", ["ngRoute", "myApp.controllers", "myApp.services"])
+angular.module("myApp", ["ngRoute", "ngResource", "myApp.controllers", "myApp.services"])
 .config(function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: "/templates/dashboard.html",
-    controller: "HomeController"
+    controller: "HomeController",
+    resolve: {
+      session: function(SessionService) {
+        return SessionService.getCurrentUser();
+      }
+    }
   })
   .otherwise({redirectTo: "/"});
 });
--- app/assets/javascripts/app/services.js
+++ app/assets/javascripts/app/services.js
@@ -13,4 +13,26 @@ angular.module("myApp.services", [])
     }
   };
   return service;
+})
+.factory("Share", function($resource) {
+  var Share = $resource("/api/shares/:id.json", {id: "@id"}, {});
+  return Share;
+})
+.factory("SessionService", function($http, $q) {
+  var service = {
+    getCurrentUser: function() {
+      if (service.isAuthenticated()) {
+        return $q.when(service.currentUser);
+      } else {
+        return $http.get("/api/current_user").then(function(resp) {
+          return service.currentUser = resp.data;
+        });
+      }
+    },
+    currentUser: null,
+    isAuthenticated: function() {
+      return !!service.currentUser;
+    }
+  };
+  return service;
 });
